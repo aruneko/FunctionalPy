15fb6be9c702a8c61434a0a34f06bc934da418a0
Yuki <aruneko99@gmail.com>
2018-03-05 16:37:32 +0900

    setup.pyの作成

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='FunctionalPy',
+    version='1.0.0',
+    description='Functional programming library for Python',
+    author='Aruneko',
+    author_email='aruneko99@gmail.com',
+    url='https://www.github.com/aruneko/functionalpy',
+    packages=['functionalpy']
+)
